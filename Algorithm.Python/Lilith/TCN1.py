472a35ee0e276cf10f2566d1126098cee585a206
Lilith River <lilith@imazen.io>
2021-04-10 14:04:07 -0600

    Empty algo with tcn

---
+++ Algorithm.Python/Lilith/TCN1.py
@@ -0,0 +1,78 @@
+from QuantConnect import *
+from QuantConnect.Parameters import *
+from QuantConnect.Benchmarks import *
+from QuantConnect.Brokerages import *
+from QuantConnect.Util import *
+from QuantConnect.Interfaces import *
+from QuantConnect.Algorithm import *
+from QuantConnect.Algorithm.Framework import *
+from QuantConnect.Algorithm.Framework.Selection import *
+from QuantConnect.Algorithm.Framework.Alphas import *
+from QuantConnect.Algorithm.Framework.Portfolio import *
+from QuantConnect.Algorithm.Framework.Execution import *
+from QuantConnect.Algorithm.Framework.Risk import *
+from QuantConnect.Indicators import *
+from QuantConnect.Data import *
+from QuantConnect.Data.Consolidators import *
+from QuantConnect.Data.Custom import *
+from QuantConnect.Data.Fundamental import *
+from QuantConnect.Data.Market import *
+from QuantConnect.Data.UniverseSelection import *
+from QuantConnect.Notifications import *
+from QuantConnect.Orders import *
+from QuantConnect.Orders.Fees import *
+from QuantConnect.Orders.Fills import *
+from QuantConnect.Orders.Slippage import *
+from QuantConnect.Scheduling import *
+from QuantConnect.Securities import *
+from QuantConnect.Securities.Equity import *
+from QuantConnect.Securities.Forex import *
+from QuantConnect.Securities.Interfaces import *
+from datetime import date, datetime, timedelta
+from QuantConnect.Python import *
+from QuantConnect.Storage import *
+from clr import AddReference
+from System import *
+from QuantConnect import *
+from QuantConnect.Algorithm import *
+import numpy as np
+import tcn
+
+
+QCAlgorithmFramework = QCAlgorithm
+QCAlgorithmFrameworkBridge = QCAlgorithm
+
+AddReference("System")
+AddReference("QuantConnect.Algorithm")
+AddReference("QuantConnect.Common")
+
+
+# <summary>
+# Basic template algorithm simply initializes the date range and cash. This is a skeleton
+# framework you can use for designing an algorithm.
+# </summary>
+# <meta name="tag" content="using data" />
+# <meta name="tag" content="using quantconnect" />
+# <meta name="tag" content="trading and orders" />
+class TCN1(QCAlgorithm):
+    """Basic template algorithm simply initializes the date range and cash"""
+
+    def Initialize(self):
+        """Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm.
+        All algorithms must initialized. """
+
+        self.SetStartDate(2013, 10, 7)  # Set Start Date
+        self.SetEndDate(2013, 10, 11)  # Set End Date
+        self.SetCash(100000)  # Set Strategy Cash
+        # Find more symbols here: http://quantconnect.com/data
+        self.AddEquity("SPY", Resolution.Minute)
+        self.Debug("numpy test >>> print numpy.pi: " + str(np.pi))
+
+    def OnData(self, data):
+        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+
+        Arguments:
+            data: Slice object keyed by symbol containing the stock data
+        '''
+        if not self.Portfolio.Invested:
+            self.SetHoldings("SPY", 1)
